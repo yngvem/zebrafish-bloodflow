710a4b05fc131f71356f20902e67f138aa37f773
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2020-11-20 13:31:19 +0100

    Added correct origin and will not copy data that is fortran-contiguouas

--- src/confocal_microscopy/vtk/pyvista_interface.py
+++ src/confocal_microscopy/vtk/pyvista_interface.py
@@ -5,6 +5,7 @@ import numpy as np
 def to_pyvista_grid(image_stack, name="Scalars_", spacing=(1, 1, 1)):
     image = pv.UniformGrid()
     image.dimensions = np.array(image_stack.shape)
+    image.origin = np.array(image_stack.shape)/2
     image.spacing = spacing
-    image.point_arrays[name] = image_stack.flatten('F')
+    image.point_arrays[name] = image_stack.ravel('F')
     return image
