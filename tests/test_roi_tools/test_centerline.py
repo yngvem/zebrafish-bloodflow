225b75d515a4873b582c02be8802fb22b13d7d82
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-11-15 10:06:30 +0100

    Improved unit tests

--- tests/test_roi_tools/test_centerline.py
+++ tests/test_roi_tools/test_centerline.py
@@ -45,19 +45,33 @@ def test_find_all_nearest_centerline_indices(rectangular_mask, rectangle_centerl
     np.testing.assert_allclose(nearest, true_nearest)
 
 
-def test_find_all_nearest_centerline_indices_from_roi(rectangular_roi, rectangle_image_shape, rectangle_centerline):
-    nearest = centerline_tools.find_all_nearest_centerline_indices_from_roi(
-        rectangular_roi, rectangle_image_shape, rectangle_centerline)
-    true_nearest = np.array([
-        [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
-        [-1,  0,  1,  2,  3,  4,  5,  6,  7,  8, -1],
-        [-1,  0,  1,  2,  3,  4,  5,  6,  7,  8, -1],
-        [-1,  0,  1,  2,  3,  4,  5,  6,  7,  8, -1],
-        [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+def test_nearest_centerline_direction(rectangular_mask, rectangle_centerline):
+    nearest = centerline_tools.find_all_nearest_centerline_indices(
+        rectangular_mask, rectangle_centerline
+    )
+    NaN = np.nan
+    true_nearest_y_direction = np.array([
+        [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN],
+        [NaN,   1,   1,   1,   1,   1,   1,   1,   1,   1, NaN],
+        [NaN,   1,   1,   1,   1,   1,   1,   1,   1,   1, NaN],
+        [NaN,   1,   1,   1,   1,   1,   1,   1,   1,   1, NaN],
+        [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN],
     ])
-    np.testing.assert_allclose(nearest, true_nearest)
+
+    true_nearest_x_direction = np.array([
+        [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN],
+        [NaN,   0,   0,   0,   0,   0,   0,   0,   0,   0, NaN],
+        [NaN,   0,   0,   0,   0,   0,   0,   0,   0,   0, NaN],
+        [NaN,   0,   0,   0,   0,   0,   0,   0,   0,   0, NaN],
+        [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN],
+    ])
+
+    nearest_direction = centerline_tools.find_nearest_centerline_direction(nearest, rectangle_centerline)
+    true_nearest_direction = np.stack([true_nearest_x_direction, true_nearest_y_direction], axis=-1)
+
+    np.testing.assert_allclose(nearest_direction, true_nearest_direction, equal_nan=True)
 
 
-def test_find_centerline(rectangular_mask, rectangle_centerline):
-    centerline = centerline_tools.find_centerline_from_mask(rectangular_mask)
-    assert False
+
+def test_find_centerline_and_clip_roi_runs(rectangular_roi, rectangle_image_shape, rectangle_centerline):
+    centerline_tools.find_centerline_and_clip_roi(rectangular_roi, rectangle_image_shape) 
